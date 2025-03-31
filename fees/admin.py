f8a4914de1007486efbeae78d47fd7f1b036d1f8
kryo <kale@kryogenic.org>
2025-03-31 16:38:52 -0600

    add command for fee payout

--- fees/admin.py
+++ fees/admin.py
@@ -25,6 +25,30 @@ from payments.models import Conversion, Coin
 log = logging.getLogger(__name__)
 
 
+privex_wallets = {
+    'BTC': 'bc1q2wjjd0fqqhf5uzy43kqyf5vsm37st8q8zuj8r4lyav3zyvvh3ytql736tu',
+    'LTC': 'LWY6hPyHP98NdZMXFKn7EvmptQnUnsNvWv',
+    'BCH': 'bitcoincash:prg0x58fkeln865q8fpse7r9zh5pyuqhdq5s5gv5k8',
+    'EOS': 'privexinceos',
+    'HBD': 'privex',
+    'DOGE': 'DAeWUsC1Kr8R1EES8XnzLJvtfAN9iLjhZu',
+    'WAX': 'privexinceos',
+    'BLURT': 'privex',
+    'STEEM': 'privex'
+}
+he_wallets = {
+    'BTC': 'bc1q324jejrpmyd23ejflhrluemsatuxa3ghuk5w3m',
+    'LTC': 'MQxmGYkWK44Lwf38KSmxj7JNSt3B4P9wae',
+    'BCH': 'bitcoincash:qznd0v0exhatqqpymtl8frqp4sfly6dmlvv8jva9hm',
+    'EOS': '',
+    'HBD': 'hive-engine',
+    'DOGE': 'D695r3CS7LM8CJSRFMRcyGFxxww1wEy9gY',
+    'WAX': '',
+    'BLURT': 'hive-engine',
+    'STEEM': 'hive-engine'
+}
+
+
 def confirm_send_payout(modeladmin, request, queryset: QuerySet):
     """
     Confirmation page for the "Suspend Services" page.
@@ -65,101 +89,99 @@ def send_payout(request):
     password = rp.get('password')
     if password != env('FEE_PAYOUT_PASS'):
         raise Exception('Invalid password supplied to send payout')
-    privex_wallets = {
-        'BTC': 'bc1q2wjjd0fqqhf5uzy43kqyf5vsm37st8q8zuj8r4lyav3zyvvh3ytql736tu',
-        'LTC': 'LWY6hPyHP98NdZMXFKn7EvmptQnUnsNvWv',
-        'BCH': 'bitcoincash:prg0x58fkeln865q8fpse7r9zh5pyuqhdq5s5gv5k8',
-        'EOS': 'privexinceos',
-        'HBD': 'privex',
-        'DOGE': 'DAeWUsC1Kr8R1EES8XnzLJvtfAN9iLjhZu',
-        'WAX': 'privexinceos',
-        'BLURT': 'privex',
-        'STEEM': 'privex'
-    }
-    he_wallets = {
-        'BTC': 'bc1q324jejrpmyd23ejflhrluemsatuxa3ghuk5w3m',
-        'LTC': 'MQxmGYkWK44Lwf38KSmxj7JNSt3B4P9wae',
-        'BCH': 'bitcoincash:qznd0v0exhatqqpymtl8frqp4sfly6dmlvv8jva9hm',
-        'EOS': '',
-        'HBD': 'hive-engine',
-        'DOGE': 'D695r3CS7LM8CJSRFMRcyGFxxww1wEy9gY',
-        'WAX': '',
-        'BLURT': 'hive-engine',
-        'STEEM': 'hive-engine'
-    }
     for d in FeePayout.objects.filter(id__in=objlist):  # type: FeePayout
-        try:
-            if d.notes == 'privex':
-                if d.coin.symbol.startswith('SWAP.'):
-                    address = 'privex'
-                else:
-                    address = privex_wallets[d.coin.symbol]
-            elif d.notes == 'hive engine':
-                if d.coin.symbol.startswith('SWAP.'):
-                    address = 'hive-engine'
-                else:
-                    address = he_wallets[d.coin.symbol]
-            else:
-                add_message(request, messages.ERROR, f'Unable to read notes during payout: {d} {d.notes}')
-                continue
-            if type(address) is tuple:
-                get_manager(d.coin.symbol_id).send(d.amount, address=address[0], memo=address[1])
-                add_message(request, messages.INFO, f"Sent {d.amount} {d.coin.symbol} to {address[0]}, memo: {address[1]}")
-                d.paid = True
-                d.save()
-            elif address:
-                get_manager(d.coin.symbol_id).send(d.amount, address=address)
-                add_message(request, messages.INFO, f"Sent {d.amount} {d.coin.symbol} to {address}")
-                d.paid = True
-                d.save()
-            else:
-                add_message(request, messages.ERROR, f"Unable to transfer {d.coin.symbol}, no address for {d.notes}")
-        except Exception as e:
-            log.exception(f'Error while paying out {d}')
-            add_message(request, messages.ERROR, f"Unable to pay out: {d} ({str(e)})")
+        status, msg = do_payout(d)
+        add_message(request, messages.INFO if status else messages.ERROR, msg)
     return redirect('/admin/fees/feepayout/')
 
 
-def get_payout(since='1970-01-01', sort=True):
-    qs_pay = FeePayout.objects.values('coin_id').filter(created_at__gt=since)
+def do_payout(d: FeePayout):
+    try:
+        if d.notes == 'privex':
+            if d.coin.symbol.startswith('SWAP.'):
+                address = 'privex'
+            else:
+                address = privex_wallets[d.coin.symbol]
+        elif d.notes == 'hive engine':
+            if d.coin.symbol.startswith('SWAP.'):
+                address = 'hive-engine'
+            else:
+                address = he_wallets[d.coin.symbol]
+        elif d.notes == 'aggroed':
+            if d.coin.symbol.startswith('SWAP.'):
+                address = 'aggroed'
+            else:
+                return False, 'Unable to distribute non SWAP coin to aggroed'
+        else:
+            return False, f'Unable to read notes during payout: {d} {d.notes}'
+        if type(address) is tuple:
+            get_manager(d.coin.symbol_id).send(d.amount, address=address[0], memo=address[1])
+            d.paid = True
+            d.save()
+            return True, f"Sent {d.amount} {d.coin.symbol} to {address[0]}, memo: {address[1]}"
+        elif address:
+            get_manager(d.coin.symbol_id).send(d.amount, address=address)
+            d.paid = True
+            d.save()
+            return True, f"Sent {d.amount} {d.coin.symbol} to {address}"
+        else:
+            return False, f"Unable to transfer {d.coin.symbol}, no address for {d.notes}"
+    except Exception as e:
+        log.exception(f'Error while paying out {d}')
+        return False, f"Unable to pay out: {d} ({str(e)} {e.__class__.__name__})"
+
+
+def get_payout(since='1970-01-01', sort=True, by_native=True):
+    qs_pay = FeePayout.objects.values('coin_id').exclude(notes__in=['aggroed', 'hive engine (surplus)']).filter(created_at__gt=since)
     qs_fee = Conversion.objects.values('from_coin_id').filter(created_at__gt=since)
+    qs_fee_se = Conversion.objects.using('steemengine').values('from_coin_id').filter(created_at__gt=since)
     payouts = {p['coin_id']: p['fee'] for p in qs_pay.annotate(fee=Sum('amount'))}
     fees = {f['from_coin_id']: f['fee'] for f in qs_fee.annotate(fee=Sum('ex_fee'))}
+    fees_se = {f['from_coin_id']: f['fee'] for f in qs_fee_se.annotate(fee=Sum('ex_fee'))}
     all_coins = [dict(coin_id=coin, amount=fees[coin]) for coin in fees.keys()] + \
+                [dict(coin_id=coin, amount=fees_se[coin], se=True) for coin in fees_se.keys()] + \
                 [dict(coin_id=coin, amount=-payouts[coin]) for coin in payouts.keys()]
     summary = {}
     for payout in all_coins:
-        coin = Coin.objects.get(symbol=payout['coin_id'])
+        coin_id = payout['coin_id']
+        ncoin_id = get_native_coin(coin_id)
+        if by_native:
+            coin_id = ncoin_id
+        co = Coin.objects
+        if payout.get('se', False):
+            co = co.using('steemengine')
+        try:
+            coin = co.get(symbol=coin_id)
+        except Coin.DoesNotExist:
+            try:
+                coin = co.get(symbol=ncoin_id)
+            except Coin.DoesNotExist:
+                continue
         if not coin.enabled:
             continue
-        coin_id = payout['coin_id']
         # collect info
         if coin_id in summary:
             if payout['amount'] > 0:
                 summary[coin_id]['fees'] += payout['amount']
-            if coin.symbol in summary[coin_id]['amounts']:
-                summary[coin_id]['amounts'][coin.symbol] += payout['amount']
+            if coin_id in summary[coin_id]['amounts']:
+                summary[coin_id]['amounts'][coin_id] += payout['amount']
             else:
-                summary[coin_id]['amounts'][coin.symbol] = payout['amount']
+                summary[coin_id]['amounts'][coin_id] = payout['amount']
         else:
-            rate = get_price(coin_id)
-            if rate == 0:
-                if 'SWAP.' in coin_id:
-                    rate = get_price(coin_id[5:])
-                if 'BTCP' == coin_id:
-                    rate = get_price('BTC')
+            rate = get_price(coin_id if by_native else ncoin_id)
             summary[coin_id] = dict(
-                amounts={coin.symbol: payout['amount']},
+                amounts={coin_id: payout['amount']},
                 rate=rate,
                 fees=max(payout['amount'], 0)
             )
+        summary[coin_id]['coin'] = coin
     # summarize
     for coin_id in summary:
         #summary[coin_id]['info'] = info[coin_id]
         summary[coin_id]['decimals'] = 4 if 'BTC' in coin_id else 2
         summary[coin_id]['amount'] = sum(summary[coin_id]['amounts'].values())
         summary[coin_id]['value'] = f"{summary[coin_id]['rate'] * Decimal(summary[coin_id]['amount']):.2f}"
-        summary[coin_id]['balances'] = get_coin_balances(Coin.objects.get(symbol=coin_id))
+        summary[coin_id]['balances'] = get_coin_balances(summary[coin_id]['coin'])
     if sort:
         return sorted(summary.items(), key=lambda i: Decimal(i[1]['value']), reverse=True)
     else:
@@ -169,8 +191,8 @@ def get_payout(since='1970-01-01', sort=True):
 def get_coin_balances(coin):
     he = Coin.objects.filter(symbol='SWAP.' + get_native_coin(coin.symbol))
     #se = Coin.objects.using('steemengine').filter(symbol=get_native_coin(coin.symbol) + 'P', coin_type='steemengine')
-    balances = [get_coin_balance(coin)]
     try:
+        balances = [get_coin_balance(coin)]
         balances += [get_coin_balance(hec, 'hive') for hec in he] + [get_coin_supply(hec, 'hive') for hec in he]
         #balances += [get_coin_balance(sec, 'steem') for sec in se] + [get_coin_supply(sec, 'steem') for sec in se]
     except Exception:
@@ -192,7 +214,15 @@ def get_coin_balance(coin, network=None):
         #return coin.symbol + ' bal', Decimal(SteemEngineToken(network=network).get_token_balance(coin.our_account, coin.symbol))
         return coin.symbol + ' steem n/a', Decimal(0)
     else:
-        return coin.symbol + ' bal', Decimal(get_manager(coin.symbol).balance(coin.our_account))
+        try:
+            bal = Decimal(get_manager(coin.symbol).balance(coin.our_account))
+            if coin.symbol == 'HBD':
+                bal += Decimal(25518.074)
+            return coin.symbol + ' bal', bal
+        except KeyError:
+            return coin.symbol + ' steem n/a', Decimal(0)
+        except Exception as e:
+            return coin.symbol + f' exc {e.__class__.__name__}', Decimal(0)
 
 
 def get_price(coin: str):
@@ -258,7 +288,8 @@ def get_unused(payout):
 
 
 def get_payout_table():
-    payout = get_payout(FeePayout.objects.values('created_at').order_by('-created_at').first()['created_at'], sort=False)
+    #payout = get_payout(FeePayout.objects.exclude(notes='aggroed').values('created_at').order_by('-created_at').first()['created_at'], sort=False)
+    payout = get_payout('1970-01-01', sort=False, by_native=False)
     payout_table = {}
     for coin, pr in payout.items():
         payout_table[coin] = dict(
@@ -279,8 +310,11 @@ def get_payout_table():
             pr['privex_cut'] = pr['total_fees'] * privex_share
             try:
                 swap_coin = Coin.objects.get(symbol_id='SWAP.' + coin)
-                pr['he_cut'] = max(pr['total_fees'] * he_share - Decimal(
-                    get_manager(swap_coin.symbol_id).balance(swap_coin.our_account)), 0)
+                try:
+                    balance = Decimal(get_manager(swap_coin.symbol_id).balance(swap_coin.our_account))
+                except KeyError:
+                    balance = Decimal(0)
+                pr['he_cut'] = max(pr['total_fees'] * he_share - balance, 0)
                 if pr['he_cut'] == 0:
                     del pr['he_cut']
             except Coin.DoesNotExist:
@@ -291,12 +325,15 @@ def get_payout_table():
             pr['value'] = ''
             try:
                 swap_coin = Coin.objects.get(symbol_id=coin)
-                pr['he_balance'] = Decimal(get_manager(swap_coin.symbol_id).balance(swap_coin.our_account))
+                try:
+                    pr['he_balance'] = Decimal(get_manager(swap_coin.symbol_id).balance(swap_coin.our_account))
+                except KeyError:
+                    pr['he_balance'] = Decimal(0)
                 pr['he_cut'] = min(payout_table[get_native_coin(coin)]['total_fees'] * he_share, pr['he_balance'])
             except Coin.DoesNotExist:
                 pass
             pr['total_fees'] = ''
-    coins = ['LTC', 'BTC', 'HBD', 'WAX', 'DOGE', 'STEEM', 'BLURT', 'BCH', 'EOS']
+    coins = ['LTC', 'BTC', 'HBD', 'DOGE', 'STEEM', 'BLURT', 'BCH']
     return filter(lambda t: t[0] in coins or get_native_coin(t[0]) in coins,
                   sorted(payout_table.items(),
                          key=lambda i: (Decimal(0.00000001) if 'SWAP' in i[0] else Decimal(1)) * i[1]['price'],
@@ -312,7 +349,7 @@ class FeePayoutView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(FeePayoutView, self).get_context_data(**kwargs)
         context['payout'] = get_payout()
-        context['payout2'] = get_payout(FeePayout.objects.order_by('-created_at')[0].created_at)
+        context['payout2'] = get_payout(FeePayout.objects.exclude(notes__in=['aggroed', 'hive engine (surplus)']).order_by('-created_at')[0].created_at)
         context['unused'] = get_unused(context['payout'])
         context['payout_table'] = get_payout_table()
         #context['prices'] = {coin: price for coin in context['payout']}
---
+++ fees/management/commands/payout_fees.py
@@ -0,0 +1,45 @@
+import sys
+from decimal import Decimal
+
+from datetime import datetime
+from django.core.management.base import BaseCommand
+
+from fees.admin import get_payout_table, do_payout, get_native_coin
+from fees.models import FeePayout
+
+import logging
+
+log = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = 'Creates and pays out FeePayouts'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'coin',
+            help='The coin to payout (optional)',
+            nargs='?'
+        )
+
+    def handle(self, *args, **options):
+        for coin, p in get_payout_table():
+            if options['coin'] and get_native_coin(coin) != options['coin'].upper():
+                self.stdout.write(self.style.NOTICE(f'Ignoring coin {coin}'))
+                continue
+            if p.get('he_cut', 0) > 0:
+                fp = FeePayout.objects.create(coin_id=coin, amount=p['he_cut'], notes='hive engine')
+            elif p.get('privex_cut', 0) > 0:
+                fp = FeePayout.objects.create(coin_id=coin, amount=p['privex_cut'], notes='privex')
+            else:
+                self.stdout.write(self.style.NOTICE('Payout amount is zero'))
+                continue
+            try:
+                status, msg = do_payout(fp)
+            except Exception as e:
+                log.exception(e)
+                self.stderr.write(self.style.ERROR(f'Unable to payout coin {coin}'))
+                continue
+            if status:
+                self.stdout.write(self.style.SUCCESS(msg))
+            else:
+                self.stderr.write(self.style.ERROR(msg))
